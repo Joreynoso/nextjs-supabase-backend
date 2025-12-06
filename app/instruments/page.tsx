585d7f12c70e59fe7fb8822440b1a7e3163c6f32
Joreynoso <joreynoso.developer@gmail.com>
2025-12-06 20:07:34 -0300

    feat: Implement instruments page with server-side data fetching and a new home page structure.

--- app/instruments/page.tsx
+++ app/instruments/page.tsx
@@ -1,12 +1,12 @@
 // ejemplo con renderizado en el servidor
 import { createClient } from '@/lib/supabase/server'
 import type { Instrument } from '@/types'
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 import { Suspense } from "react";
+import InstrumentsList from '@/components/instrumentsList'
 
 export default async function PageInstruments() {
   const supabase = await createClient()
-  const { data: instruments } = await supabase.from('instruments').select()
+  const { data: instruments }  = await supabase.from('instruments').select()
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
@@ -15,15 +15,7 @@ export default async function PageInstruments() {
       <h1 className='text-2xl font-bold'>Instruments</h1>
       <p className='text-sm text-muted-foreground mb-10'>Your instruments, this section has a private policy from supabase.</p>
 
-      <div className='w-full max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-        {instruments?.map((instrument: Instrument) => (
-          <Card key={instrument.id} className='bg-card rounded-lg shadow-md p-4'>
-            <CardHeader>
-              <CardTitle>{instrument.name}</CardTitle>
-            </CardHeader>
-          </Card>
-        ))}
-      </div>
+      <InstrumentsList instruments={instruments as Instrument[]} />
     </div>
     </Suspense>
   )
